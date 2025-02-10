2470027850275e8df667877c6c0f835220eac1cc
TitingSchleiden99 <samson.renzanthony.schleiden@gmail.com>
2025-02-10 13:33:25 +0800

    Update main.py

--- main.py
+++ main.py
@@ -12,3 +12,7 @@ for i in range (100):
         t.right(90)
         t.left(45)
         pattern+=1
+        #This is an octagon
+        #Black and Blue are it's main colors
+        #It is a pattern
+        #It follows an arrow
